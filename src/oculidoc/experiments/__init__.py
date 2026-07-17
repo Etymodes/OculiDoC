644cd9ef4277d992fe387f50441234e93fd95b73
Etymodes <112923230+Etymodes@users.noreply.github.com>
2026-07-17 13:25:05 +0800

    Add normalized gaze experiment recorder

---
+++ src/oculidoc/experiments/__init__.py
@@ -0,0 +1,17 @@
+"""Experiment recording and analysis primitives."""
+
+from oculidoc.experiments.recording import (
+    AoiRole,
+    NormalizedAoi,
+    RecorderState,
+    ScreenContext,
+    TaskRunRecorder,
+)
+
+__all__ = [
+    "AoiRole",
+    "NormalizedAoi",
+    "RecorderState",
+    "ScreenContext",
+    "TaskRunRecorder",
+]
